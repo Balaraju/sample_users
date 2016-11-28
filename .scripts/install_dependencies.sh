527939563f9862b2da6f4c970f15bc9d3f052015
BalaRaju <vankalabalaraju@gmail.com>
2016-11-28 12:40:16 +0530

    Update install_dependencies.sh

--- .scripts/install_dependencies.sh
+++ .scripts/install_dependencies.sh
@@ -1,1 +1,6 @@
-insatall mysql
+#!/usr/bin/env bash
+
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password p@ssw0rd'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password p@ssw0rd'
+apt-get update
+apt-get install -y mysql-server
